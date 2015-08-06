2af9518605b065de67423a1f161b55c2d948a607
Nicolas Eypert <rastermax@gmail.com>
2015-08-06 16:52:31 -0400

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by neypert-mrc on 2015-06-29.
+ */
+
+//WRONG WAY
+    //var colors =require("path to // ...colors.js");
+
+//GOOD
+// USE NPM TO GET MODULE
+
+var   colors = require("colors");
+
+//creer le oldpackage.json avec npm init
+//installer avec npm install
+
+
+console.log("toto".red);
+console.log("toto".blue);
+console.log("toto".yellow);
+
